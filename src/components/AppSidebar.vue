8bad8af3c6f652a627223c4cf0f61c438f8f13f5
Benjamin Romero <programador.ben@gmail.com>
2025-11-15 15:48:46 -0400

    feat: :lipstick: agregado sidebar básico

---
+++ src/components/AppSidebar.vue
@@ -0,0 +1,56 @@
+<script setup lang="ts">
+import { Home, Inbox } from 'lucide-vue-next'
+import { useRoute } from 'vue-router'
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from '@/components/ui/sidebar'
+
+const route = useRoute()
+
+// Menu items.
+const items = [
+  {
+    title: 'Home',
+    url: '/',
+    icon: Home,
+  },
+  {
+    title: 'About',
+    url: '/about',
+    icon: Inbox,
+  },
+]
+
+const isActive = (url: string) => {
+  return route.path === url
+}
+</script>
+
+<template>
+  <Sidebar>
+    <SidebarContent>
+      <SidebarGroup>
+        <SidebarGroupLabel>Nombre de la app</SidebarGroupLabel>
+        <SidebarGroupContent>
+          <SidebarMenu>
+            <SidebarMenuItem v-for="item in items" :key="item.title">
+              <SidebarMenuButton asChild :isActive="isActive(item.url)">
+                <RouterLink :to="item.url" class="flex items-center gap-2">
+                  <component :is="item.icon" />
+                  <span>{{ item.title }}</span>
+                </RouterLink>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+          </SidebarMenu>
+        </SidebarGroupContent>
+      </SidebarGroup>
+    </SidebarContent>
+  </Sidebar>
+</template>
---
+++ src/layouts/MyLayout.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar'
+import AppSidebar from '@/components/AppSidebar.vue'
+import { useRoute } from 'vue-router'
+import { computed } from 'vue'
+
+// Título dinámico desde meta.title
+const route = useRoute()
+const pageTitle = computed(() => route.meta.title ?? 'Página')
+</script>
+
+<template>
+  <SidebarProvider>
+    <AppSidebar />
+
+    <main class="flex-1 flex flex-col min-h-screen">
+      <!-- Encabezado -->
+      <header class="border-b px-6 py-4 bg-background flex items-center gap-3">
+        <SidebarTrigger />
+        <h1 class="text-xl font-semibold">{{ pageTitle }}</h1>
+      </header>
+
+      <!-- Contenido -->
+      <section class="flex-1 p-6">
+        <div class="max-w-6xl mx-auto">
+          <RouterView />
+        </div>
+      </section>
+    </main>
+  </SidebarProvider>
+</template>
