6e11b37389ca7e064ef798be1202245bd2ae34e6
Benjamin Romero <programador.ben@gmail.com>
2025-11-15 15:12:56 -0400

    build: :heavy_plus_sign: agregado tailwind y shadcn

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+// import TheWelcome from '../components/TheWelcome.vue'
+import { Button } from '@/components/ui/button'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <!-- <TheWelcome /> -->
+    <Button>Click me</Button>
   </main>
 </template>
